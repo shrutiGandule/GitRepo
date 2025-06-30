7a7df9edd3bbd8ba03bfb946819977bdca871f2c
shrutigandule24@gmail.com <shrutigandule24@gmail.com>
2025-06-30 23:05:29 +0530

    new

--- demoGit/src/app/app.module.ts
+++ demoGit/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MotorIncComponent } from './motor-inc/motor-inc.component';
+import { TravelIncComponent } from './travel-inc/travel-inc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MotorIncComponent,
+    TravelIncComponent
   ],
   imports: [
     BrowserModule
