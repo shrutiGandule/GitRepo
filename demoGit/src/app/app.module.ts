f578939a2ec3ce2149795238e035affb8dc17e6b
shrutigandule24@gmail.com <shrutigandule24@gmail.com>
2025-07-01 10:41:12 +0530

    test

--- demoGit/src/app/app.module.ts
+++ demoGit/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MotorIncComponent } from './motor-inc/motor-inc.component';
-import { TravelIncComponent } from './travel-inc/travel-inc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MotorIncComponent,
-    TravelIncComponent
+  
   ],
   imports: [
     BrowserModule
